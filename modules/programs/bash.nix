121b855b29dad29d842177d551f104accb8e3220
David Guibert <david.guibert@gmail.com>
2022-03-25 18:55:09 +0100

    bash: handle infinite history sizes

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -51,7 +51,7 @@ in {
       };
 
       historySize = mkOption {
-        type = types.int;
+        type = types.ints.s32;
         default = 10000;
         description = "Number of history lines to keep in memory.";
       };
@@ -63,7 +63,7 @@ in {
       };
 
       historyFileSize = mkOption {
-        type = types.int;
+        type = types.ints.s32;
         default = 100000;
         description = "Number of history lines to keep on file.";
       };
