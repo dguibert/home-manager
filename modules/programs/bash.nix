3ca047beff68f2801f982e8ac6a42f5b0b32336e
David Guibert <david.guibert@gmail.com>
2022-04-18 11:52:48 +0200

    histignore: quote value

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -181,7 +181,7 @@ in
             HISTCONTROL = concatStringsSep ":" cfg.historyControl;
           }
           // optionalAttrs (cfg.historyIgnore != []) {
-            HISTIGNORE = concatStringsSep ":" cfg.historyIgnore;
+            HISTIGNORE = "\"${concatStringsSep ":" cfg.historyIgnore}\"";
           }
         ));
     in mkIf cfg.enable {
